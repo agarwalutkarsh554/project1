af4eb0c22fe166b116d4a3fe24e400dd8ba84e97
Utkarsh <agarwalutkarsh554@gmail.com>
2023-07-21 00:31:44 +0530

    modified:   blog/__pycache__/urls.cpython-311.pyc 	modified:   blog/__pycache__/views.cpython-311.pyc 	modified:   blog/urls.py 	modified:   blog/views.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.Home,name='blog-home'),
+    path('about/',views.About,name='blog-about')
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 
 def Home(request):
     return HttpResponse('<h1>Blog-Home</h1>')
+def About(request):
+    return HttpResponse('<h1>About</h1>')
+
 
 
 # Create your views here.
