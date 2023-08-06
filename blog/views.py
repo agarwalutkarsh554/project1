08654062ba101514c2fff7831e18c049cf618d64
Utkarsh <agarwalutkarsh554@gmail.com>
2023-08-06 22:07:30 +0530

    modified:   project1/urls.py

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import post#. here means that it is in the same package if it was in some other package we would have mentioned that before .
 def Home(request):
-    return render(request,'blog/home.html')
+    context={
+        'posts':post.objects.all()
+    }
+    return render(request, 'blog/home.html',context)
 def About(request):
-    return HttpResponse('<h1>About</h1>')
+
+    return render(request, 'blog/about.html',{'title':'About'})
 
 
 
